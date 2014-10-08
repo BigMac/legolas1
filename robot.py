160ecef13d8a476d599ee81285488003955f3d2e
Legolas <brick@lego>
2014-10-08 12:52:30 +0000

    fixes and logs, doesnt work

--- robot.py
+++ robot.py
@@ -25,47 +25,54 @@ class Robot:
         pass
 
     def drive(self, distance_mm, block_until_done=True):
+        print('drive '+str(distance_mm) + 'mm')
         distance_rotations_ratio = 360/103.0
         rel_position = distance_mm * distance_rotations_ratio;
         power = 600
         ramp = 1000
-        _rotate_track_rel(self.left_track, rel_position, power, ramp)
-        _rotate_track_rel(self.right_track, rel_position, power, ramp)
+        self._rotate_track_rel(self.left_track, rel_position, power, ramp)
+        self._rotate_track_rel(self.right_track, rel_position, power, ramp)
         if (block_until_done):
-          return self.move_until_finished()
+            return self.move_until_finished()
         else:
-          return True
+            return True
 
     def turn(self, degrees, block_until_done=True):
+        print('turn '+str(degrees))
         ratio = 555/90.0
         rel_position = ratio * degrees;
         power = 400
         ramp = 400
-        _rotate_track_rel(self.left_track, rel_position, power, ramp)
-        _rotate_track_rel(self.right_track, -rel_position, power, ramp)
+        self._rotate_track_rel(self.left_track, rel_position, power, ramp)
+        self._rotate_track_rel(self.right_track, -rel_position, power, ramp)
         if (block_until_done):
-          return self.move_until_finished()
+            return self.move_until_finished()
         else:
-          return True
+            return True
 
     def motors_running(self):
-      return self.left_track.run || self.right_track.run
+        return self.left_track.run or self.right_track.run
 
     def stop(self):
-      self.left_track.stop()
-      self.right_track.stop()
+        self.left_track.stop()
+        self.right_track.stop()
+        self.left_track.reset()
+        self.right_track.reset()
 
     def move_until_finished(self):
-      while (self.motors_running()):
-        if (self.distance_front() < 10):
-          self.stop()
-          return False  # Was interrupted
-        else:
-          time.sleep(0.1)
-      return True  # Finished as planned
+        print('move until finished')
+        while (self.motors_running()):
+            if (self.distance_front() < 10):
+                self.stop()
+                print('action interrupted')
+                return False  # Was interrupted
+            else:
+                print('motors still running ' + str(self.motors_running()))
+                time.sleep(0.1)
+        return True  # Finished as planned
 
     def _rotate_track_rel(self, track, rel_position, power, ramp):
-      track.position_mode=Motor.POSITION_MODE.RELATIVE)
+      track.position_mode=Motor.POSITION_MODE.RELATIVE
       track.run_position_limited(position_sp=rel_position,
                                  speed_sp=power,
                                  stop_mode=Motor.STOP_MODE.BRAKE,
