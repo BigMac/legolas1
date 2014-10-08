94550c8b816bce7b5e872daf6082e064afe6a66f
Tomasz Śniatowski <tsniatowski@opera.com>
2014-10-08 20:14:02 +0200

    key fix 3

--- key.py
+++ key.py
@@ -5,13 +5,8 @@ from ev3.ev3dev import Key
 def program():
     r = robot.Robot()
     while True:
-        k = Key()
-        print(str(k.up))
-        print(str(k.__dict__))
-        time.sleep(500)
-
-    while True:
         r.keys()
+        time.sleep(500)
 
 if __name__ == '__main__':
     program()
