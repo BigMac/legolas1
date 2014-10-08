c1af98242c2fcc5c6558d515760079353b33bae4
Legolas <brick@lego>
2014-10-08 18:24:43 +0000

    key upgrade

--- key.py
+++ key.py
@@ -1,12 +1,21 @@
 import robot
+import program
 import time
+import wall
+import os
 from ev3.ev3dev import Key
 
 def program():
     r = robot.Robot()
+    k = Key()
     while True:
-        r.keys()
-        time.sleep(500)
+        time.sleep(0.1)
+        if k.up:
+            wall.program()
+            time.sleep(1)
+        if k.backspace:
+            os.system("shutdown -h 0")   
+	
 
 if __name__ == '__main__':
     program()
--- robot.py
+++ robot.py
@@ -27,7 +27,7 @@ class Robot:
 
     def keys(self):
         keys = ('up', 'down', 'left', 'right', 'backspace', 'enter')
-        dump = [x + ":" + str(gettatr(self.key, x)) for x in keys]
+        dump = [x + ":" + str(getattr(self.key, x)) for x in keys]
         print " ".join(dump)
         return self.key
 
