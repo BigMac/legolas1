0186503ef7fe8011e79d4812ef5720b3390c5293
Tomasz Śniatowski <tsniatowski@opera.com>
2014-10-08 20:03:44 +0200

    key fix 1

--- key.py
+++ key.py
@@ -1,11 +1,17 @@
 import robot
 import time
+from ev3.ev3dev import Key
 
 def program():
     r = robot.Robot()
     while True:
-        r.keys()
+        k = Key()
+        print(str(k.up))
+        print(str(k.__dict__))
         time.sleep(500)
+
+    while True:
+        r.keys()
 
 if __name__ == '__main__':
     program()
